4b9fbfb033ffce9e41075f0e8b8f20c4811f8478
lamtandai2002 <daitanlam2002@gmail.com>
2025-02-28 22:34:04 +0700

    commit-v2

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,9 +1,21 @@
 package vn.hoidanit.laptopshop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    @GetMapping("/")
+	@GetMapping("/")
 	public String index() {
-		return "Hello World from Spring Boot!";
+		return "Hello World!";
+	}
+	@GetMapping("/user")
+	public String user() {
+		return "Hello user!";
+	}
+	@GetMapping("/admin")
+	public String admin() {
+		return "Hello admin!";
 	}
 
 }
